d9099df9ba84384211f9a403440734d73543624c
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-03 00:27:49 +0200

    added the screens

--- calculator/setup/src/components/index.js
+++ calculator/setup/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Wrapper } from './Wrapper'
 export { default as Button } from './Button'
 export { default as ButtonBox } from './ButtonBox'
+export { default as Screens } from './Screens'
